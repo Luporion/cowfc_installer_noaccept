04fb5df160202afb47b65cd2e9f452b0f47d2120
Luporion <98816560+Luporion@users.noreply.github.com>
2024-08-09 00:23:49 +0200

    Update cowfc.sh change update link
    Changed UPDATE_BASE to my version of cowfc.sh

--- cowfc.sh
+++ cowfc.sh
@@ -48,7 +48,7 @@ mod1="proxy"      # This is a proxy mod that is dependent on the other 2
 mod2="proxy_http" # This is related to mod1
 mod3="php7.4"
 UPDATE_FILE="$0.tmp"
-UPDATE_BASE="https://raw.githubusercontent.com/EnergyCube/cowfc_installer/master/cowfc.sh"
+UPDATE_BASE="https://raw.githubusercontent.com/Luporion/cowfc_installer_noaccept/master/cowfc.sh"
 # Functions
 
 function update() {
